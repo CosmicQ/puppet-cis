06a15c25a5ffa1d54bd61339ec73b70e346f3a2b
Jason Qualkenbush <jason.qualkenbush@nuance.com>
2018-04-09 10:55:17 -0400

    Moving 3.x into network.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -63,12 +63,6 @@ class cis (
   }
 
   # 3 - Network Configuration
-  include cis::network::net_3_1
-  include cis::network::net_3_2
-  include cis::network::net_3_3
-  include cis::network::net_3_4
-  include cis::network::net_3_5
-  include cis::network::net_3_6
-  include cis::network::net_3_7
+  include cis::network
 
 }
