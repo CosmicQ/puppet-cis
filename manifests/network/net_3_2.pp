3782b30f84ed92adeefe5c34a4e7bb2dc3e548f8
Jason Qualkenbush <jason.qualkenbush@nuance.com>
2018-04-09 11:27:18 -0400

    Fixing the key/value

--- manifests/network/net_3_2.pp
+++ manifests/network/net_3_2.pp
@@ -18,19 +18,19 @@ class cis::network::net_3_2 (
   if $check {
     # 3.2.1 - 3.2.8
     $kernel_parameters = {
-      'net.ipv4.conf.all.accept_source_route' => { value => '0' },
-      'net.ipv4.conf.default.accept_source_route' => { value => '0' },
-      'net.ipv4.conf.all.accept_redirects' => { value => '0' },
-      'net.ipv4.conf.default.accept_redirects' => { value => '0' },
-      'net.ipv4.conf.all.secure_redirects' => { value => '0' },
-      'net.ipv4.conf.default.secure_redirects' => { value => '0' },
-      'net.ipv4.conf.all.log_martians' => { value => '1' },
-      'net.ipv4.conf.default.log_martians' => { value => '1' },
-      'net.ipv4.icmp_echo_ignore_broadcasts' => { value => '1' },
-      'net.ipv4.icmp_ignore_bogus_error_responses' => { value => '1' },
-      'net.ipv4.conf.all.rp_filter' => { value => '1' },
-      'net.ipv4.conf.default.rp_filter' => { value => '1' },
-      'net.ipv4.tcp_syncookies' => { value => '1' },
+      'net.ipv4.conf.all.accept_source_route' => '0',
+      'net.ipv4.conf.default.accept_source_route' => '0',
+      'net.ipv4.conf.all.accept_redirects' => '0',
+      'net.ipv4.conf.default.accept_redirects' => '0',
+      'net.ipv4.conf.all.secure_redirects' => '0',
+      'net.ipv4.conf.default.secure_redirects' => '0',
+      'net.ipv4.conf.all.log_martians' => '1',
+      'net.ipv4.conf.default.log_martians' => '1',
+      'net.ipv4.icmp_echo_ignore_broadcasts' => '1',
+      'net.ipv4.icmp_ignore_bogus_error_responses' => '1',
+      'net.ipv4.conf.all.rp_filter' => '1',
+      'net.ipv4.conf.default.rp_filter' => '1',
+      'net.ipv4.tcp_syncookies' => '1',
     }
 
     $kernel_parameters.each |String $k_param, Hash $val| {
