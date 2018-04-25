8eaa6a7b0c28a9f01f5ec9077e278957a9fcc217
Jason Qualkenbush <jason.qualkenbush@nuance.com>
2018-04-25 14:39:49 -0400

    fixing var formatting

--- manifests/setup/set_1_1.pp
+++ manifests/setup/set_1_1.pp
@@ -1,73 +1,62 @@
-class cis::setup::set_1_1 {
+class cis::setup::set_1_1 (
 
-  $filesystem_list = [
-    'cramfs',
-    'freevxfs',
-    'jffs2',
-    'hfs',
-    'hfsplus',
-    'squashfs',
-    'udf',
-    'vfat'
-  ]
+  String $status            = 'disable',
+  Array  $filesystem_list   = undef,
+  Hash   $filesystem_mounts = undef
 
+){
+
+  $check = $status ? {
+    'enable'  => true,
+    'notify'  => true,
+    default   => false,
+  }
+
+  $run = $status ? {
+    'enable'  => false,
+    default   => true,
+  }
+
+  # Filesystem List is found in data/setup.yaml
   $filesystem_list.each | String $filesystem | {
     file_line { $filesystem:
-      ensure => present,
-      path   => '/etc/modprobe.d/CIS.conf',
-      line   => "install ${filesystem} /bin/true",
-      noop   => $run,
+      ensure  => present,
+      path    => '/etc/modprobe.d/CIS.conf',
+      line    => "install ${filesystem} /bin/true",
+      noop    => $run,
+    }
+  }
+
+  # Filesystem Mounts are found in data/setup.yaml
+  $filesystem_mounts.each | String $mount, String $options | {
+    mount { $mount:
+      options => $options,
+      noop    => $run,
     }
   }
 
   file {'/etc/fstab':
-    owner => root,
-    group => root,
-    mode  => '0600',
-  }
-
-  mount {'/tmp':
-    options => 'nodev,nosuid,noexec',
-  }
-
-  mount {'/var':
-    options => 'nodev',
-  }
-
-  mount {'/var/log':
-    options => 'nodev',
-  }
-
-  mount {'/var/log/audit':
-    options => 'nodev',
-  }
-
-  mount {'/home':
-    options => 'nodev',
-  }
-
-  mount {'/opt':
-    options => 'nodev',
-  }
-
-  mount {'/dev/shm':
-    options => 'nodev,nosuid,noexec',
+    owner     => root,
+    group     => root,
+    mode      => '0600',
+    noop      => $run,
   }
 
   # 1.1.6
   mount {'/var/tmp':
-    ensure  => 'mounted',
-    device  => '/tmp',
-    fstype  => 'none',
-    options => 'bind',
+    ensure    => 'mounted',
+    device    => '/tmp',
+    fstype    => 'none',
+    options   => 'bind',
+    noop      => $run,
   }
 
   file {'/tmp':
-    ensure => directory,
-    owner  => root,
-    group  => root,
-    mode   => '1777',
+    ensure    => directory,
+    owner     => root,
+    group     => root,
+    mode      => '1777',
+    noop      => $run,
   }
-
 
 }
