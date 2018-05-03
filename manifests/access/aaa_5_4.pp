edbde4ab0c6f27473a42ea7c21279f700ab97f90
CosmicQ <jason@cosmicegg.net>
2018-05-02 21:37:25 -0400

    Adding to 5.4

--- manifests/access/aaa_5_4.pp
+++ manifests/access/aaa_5_4.pp
@@ -23,17 +23,45 @@ class cis::access::aaa_5_4 (
 #PASS_MAX_DAYS 90
 #PASS_MIN_DAYS 7
 #PASS_WARN_AGE 7
+
+  $login_defs.each | String $param, String $val | {
+    file_line { 'login_defs':
+        ensure  => present,
+        path    => '/etc/login_defs',
+        line    => "${param} ${val}",
+        match   => "^${param}",
+        noop    => $run,      
+    }
+  }
+
 #INACTIVE=30
 
 # 5.4.2 - System account are non-login
 
 #root gid = 0
 
+  user { 'root':
+    ensure => persent,
+    uid    => 0,
+    gid    => 0,
+    noop   => $run,
+  }
+
 # 5.4.4
 # grep "^umask" /etc/bashrc
 #umask 027
 # grep "^umask" /etc/profile
 #umask 027
+
+  $umask.each | String $param, String $val | {
+    file_line { 'umask':
+        ensure  => present,
+        path    => '/etc/login_defs',
+        line    => "${param} ${val}",
+        match   => "^${param}",
+        noop    => $run,      
+    }
+  }
 
 
 
