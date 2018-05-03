bb4aaab9245a990495bcecc67c99eae224fc009c
Jason Qualkenbush <jason.qualkenbush@nuance.com>
2018-05-03 12:06:34 -0400

    Adding the header stuff

--- manifests/windows/scm.pp
+++ manifests/windows/scm.pp
@@ -1,16 +1,37 @@
-class cis::windows::scm {
+class cis::windows::scm   (
 
-  # This is all registry crap.  We can do this with hiera and a sweet function.
-  
-  $scm_registry.each | String $name, Hash $values | {
-    dsc_registry { $name:
-      dsc_ensure    => 'Present',
-      dsc_key       => $values[key],
-      dsc_valuename => $values[valuename],
-      dsc_valuetype => $values[valuetype],
-      dsc_valuedata => $values[valuedata],
-    }
+  String $status,
+  Hash   $scm_registry,
+
+){
+
+  $check = $status ? {
+    'enable'  => true,
+    'notify'  => true,
+    default   => false,
   }
- 
 
+  $run = $status ? {
+    'enable'  => false,
+    default   => true,
+  }
+
+  $defaults = {
+    noop  => $run,
+  }
+
+  if $check {
+
+    # This is all registry crap.  We can do this with hiera and a sweet function.
+    $scm_registry.each | String $name, Hash $values | {
+      dsc_registry { $name:
+        dsc_ensure    => 'Present',
+        dsc_key       => $values[key],
+        dsc_valuename => $values[valuename],
+        dsc_valuetype => $values[valuetype],
+        dsc_valuedata => $values[valuedata],
+      }
+    }
+
+  }
 }
