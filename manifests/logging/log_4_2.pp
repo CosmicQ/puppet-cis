e038447c01d70fd7bce65a0bb3a3a0b88b8ce168
Jason Qualkenbush <jason.qualkenbush@nuance.com>
2018-05-03 08:45:10 -0400

    Moving Redhat CIS to the RedHat.yaml hiera

--- manifests/logging/log_4_2.pp
+++ manifests/logging/log_4_2.pp
@@ -16,9 +16,28 @@ class cis::loging::log_4_2 (
     default  => true,
   }
 
-#  if $check {
+  if $check {
+    # 4.2.3
+    package { 'cis-log':
+      ensure => present,
+      name   => 'rsyslog',
+      noop   => $run,
+    }
+    # 4.2.1.1
+    service { 'cis-log':
+      ensure => running,
+      name   => 'rsyslog',
+      noop   => $run,
+    }
 
+    # 4.2.1.3
+    file_line { 'FileCreateMode':
+      ensure => present,
+      path   => '/etc/rsyslog.conf',
+      line   => "$FileCreateMode 0640",
+      match  => "^$FileCreateMode",
+      noop   => $run,
+    }
 
-
-#  }
+  }
 }
